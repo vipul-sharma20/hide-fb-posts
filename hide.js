824d812274b82d5b7c45535d551143b11ce590cc
Vipul Sharma <vipul.sharma20@gmail.com>
2016-05-15 22:57:43 +0530

    added code to hide posts

---
+++ hide.js
@@ -0,0 +1,22 @@
+a = document.querySelectorAll('div._1dwg');
+var check = [/two names/, /inbox/, /be honest/, /honest comments/, /without mentioning their names/, /like my status/];
+
+for(var i=0; i<a.length;i++)
+{
+    var len = check.length,
+    j = 0;
+    var count = 0;
+    s = a[i].innerHTML.toLowerCase();
+
+    for (; j < len; j++) {
+        if (s.match(check[j])) {
+            count++;
+        }
+    }
+    
+    if (count >= 3)
+    {
+        a[i].parentElement.parentElement.style.visibility = 'hidden';
+    }
+
+}
